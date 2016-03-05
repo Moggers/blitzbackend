dc6b081098c064e12a160fbab4a717e362967cab
root <root@blitzserver.net>
2016-03-05 04:34:01 +0000

    Autostale

--- src/match.cpp
+++ src/match.cpp
@@ -109,7 +109,7 @@ namespace Game
 		if( this->hosthour != 0 || this->hostday != 0 ) {
 			sprintf( str + strlen( str ), "--hosttime %d %d", this->hostday, this->hosthour );
 		}
-		sprintf( str + strlen( str ), "--renaming %d -dd --research %d --era %d --thrones %d %d %d --requiredap %d --mapfile \"%s\" \"%s%lu\"", 
+		sprintf( str + strlen( str ), "--maxholdups 1 --renaming %d -dd --research %d --era %d --thrones %d %d %d --requiredap %d --mapfile \"%s\" \"%s%lu\"", 
 			this->renaming,
 			this->research,
 			this->age, 
